bec0d44b3f99bc7c3c17faed032e972de1923278
Harsh Shah <hash.neon@gmail.com>
2013-05-23 19:00:42 +0530

    second commit

--- myscript.py
+++ myscript.py
@@ -5,7 +5,7 @@ def main(parameters):
     numpy.random.seed(parameters["seed"])
     distr = getattr(numpy.random, parameters["distr"])
     data = distr(size=parameters["n"])
-    output_file = "example.dat"
+    output_file = "output"
     numpy.savetxt(output_file, data)
 
 parameter_file = sys.argv[1]
